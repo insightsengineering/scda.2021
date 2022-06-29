c9b0da573a96394e092b79ea2132d9522ef64a89
arkadiusz.beer <arkadiusz.beer@contractors.roche.com>
2022-06-29 12:42:02 +0200

    test

--- R/data.R
+++ R/data.R
@@ -1,21 +1,20 @@
 #' Cached datasets from `random.cdisc.data`
 #'
-#' Cached datasets (test3) from 2021 releases of `random.cdisc.data`.
+#' Cached datasets (test4) from 2021 releases of `random.cdisc.data`.
 #'
 #' @name rcd_data_2021
 #'
 NULL
 
 #' @rdname rcd_data_2021
-
-"rcd_2021_03_22"
+  "rcd_2021_03_22"
 
 
 
 
 
 #' @rdname rcd_data_2021
-"rcd_2021_05_05"
+     "rcd_2021_05_05"
 
 
 #' @rdname rcd_data_2021
