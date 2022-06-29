78592ff1e84a4eafa07ad6e71054df31c42aab4d
arkadiusz.beer <arkadiusz.beer@contractors.roche.com>
2022-06-29 13:40:25 +0200

    [skip r-cmd][skip gitleaks][skip licenses][skip links][skip linter][skip docs][skip spelling][skip coverage][skip validation][skip vbump][skip version][skip bioccheck][skip audit]

--- R/data.R
+++ R/data.R
@@ -1,6 +1,6 @@
 #' Cached datasets from `random.cdisc.data`
 #'
-#' Cached datasets (test7) from 2021 releases of `random.cdisc.data`.
+#' Cached datasets (test8) from 2021 releases of `random.cdisc.data`.
 #'
 #' @name rcd_data_2021
 #'
