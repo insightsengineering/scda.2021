80e7793d6178eb9e90a6a1c0396ffbad24bac78e
Emily de la Rua <59304861+edelarua@users.noreply.github.com>
2022-09-30 09:18:59 -0400

    98 Create separate files for `scda.2021` datasets (#93)
    * Update rcd and documentation
    
    * Add new data files
    
    * Allow update of full dataset files
    
    * Keep full dataset files
    
    * [skip actions] Restyle files
    
    * [skip actions] Roxygen Man Pages Auto Update
    
    * style, update filepath
    
    Co-authored-by: github-actions <41898282+github-actions[bot]@users.noreply.github.com>
    Co-authored-by: 27856297+dependabot-preview[bot]@users.noreply.github.com <27856297+dependabot-preview[bot]@users.noreply.github.com>

---
+++ R/data_2021_07_07.R
@@ -0,0 +1,53 @@
+#' Cached datasets from the 2021_07_07 release of `random.cdisc.data`
+#'
+#' @name rcd_data_2021_07_07
+#'
+NULL
+
+#' @rdname rcd_data_2021_07_07
+"rcd_2021_07_07_adsl"
+
+#' @rdname rcd_data_2021_07_07
+"rcd_2021_07_07_adae"
+
+#' @rdname rcd_data_2021_07_07
+"rcd_2021_07_07_adaette"
+
+#' @rdname rcd_data_2021_07_07
+"rcd_2021_07_07_adcm"
+
+#' @rdname rcd_data_2021_07_07
+"rcd_2021_07_07_addv"
+
+#' @rdname rcd_data_2021_07_07
+"rcd_2021_07_07_adeg"
+
+#' @rdname rcd_data_2021_07_07
+"rcd_2021_07_07_adex"
+
+#' @rdname rcd_data_2021_07_07
+"rcd_2021_07_07_adhy"
+
+#' @rdname rcd_data_2021_07_07
+"rcd_2021_07_07_adlb"
+
+#' @rdname rcd_data_2021_07_07
+"rcd_2021_07_07_admh"
+
+#' @rdname rcd_data_2021_07_07
+"rcd_2021_07_07_adqs"
+
+#' @rdname rcd_data_2021_07_07
+"rcd_2021_07_07_adrs"
+
+#' @rdname rcd_data_2021_07_07
+"rcd_2021_07_07_adsub"
+
+#' @rdname rcd_data_2021_07_07
+"rcd_2021_07_07_adtr"
+
+#' @rdname rcd_data_2021_07_07
+"rcd_2021_07_07_adtte"
+
+#' @rdname rcd_data_2021_07_07
+"rcd_2021_07_07_advs"
